86e330d9c1c4f939916be6890075148a96380f15
Jentsch <d@jentsch.berlin>
2016-08-09 06:34:33 +0200

    QA-9 formated all scala files with `sbt scalafmt`
    Change-Id: Iac2dc4a0108e3ccd0a1172799b9a7762bc827fbf

--- app/controllers/BackendController.scala
+++ app/controllers/BackendController.scala
@@ -15,17 +15,20 @@ import play.api.mvc._
 class BackendController @Inject() extends Controller {
 
   case class ClientUpdate(id: Int,
-                          lat: Double, lng: Double, ele: Double, heading: Double,
+                          lat: Double,
+                          lng: Double,
+                          ele: Double,
+                          heading: Double,
                           timestamp: Int)
 
   implicit val clientUpdate: Reads[ClientUpdate] = (
-    (JsPath \ "id").read[Int] and
-    (JsPath \ "lat").read[Double](min(-90.0) keepAnd max(90.0)) and
-    (JsPath \ "lng").read[Double](min(-180.0) keepAnd max(180.0)) and
-    (JsPath \ "ele").read[Double](min(-1000.0) keepAnd max(8000.0)) and
-    (JsPath \ "heading").read[Double](min(0.0) keepAnd max(360.0)) and
-    (JsPath \ "timestamp").read[Int](min(0))
-    ) (ClientUpdate.apply _)
+      (JsPath \ "id").read[Int] and
+      (JsPath \ "lat").read[Double](min(-90.0) keepAnd max(90.0)) and
+      (JsPath \ "lng").read[Double](min(-180.0) keepAnd max(180.0)) and
+      (JsPath \ "ele").read[Double](min(-1000.0) keepAnd max(8000.0)) and
+      (JsPath \ "heading").read[Double](min(0.0) keepAnd max(360.0)) and
+      (JsPath \ "timestamp").read[Int](min(0))
+  )(ClientUpdate.apply _)
 
   def send = Action(BodyParsers.parse.json[Seq[ClientUpdate]]) { request =>
     val coordinates = request.body
