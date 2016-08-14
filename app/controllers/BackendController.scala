be6c504a2370362d6869db607014c407536f77b4
Jentsch <d@jentsch.berlin>
2016-08-14 19:40:55 +0200

    BE-23 Backend sends and stores track-points from clients +Mehmed
    Change-Id: I4eaf18287a3393c71d28e97e81068c2191969256

--- app/controllers/BackendController.scala
+++ app/controllers/BackendController.scala
@@ -3,28 +3,20 @@ package controllers
 import java.util.UUID
 import javax.inject._
 
-import play.api.libs.functional.syntax._
-import play.api.libs.json.Reads._
-import play.api.libs.json._
+import akka.stream.Materializer
+import models.TrackPoint
 import play.api.mvc._
+import services.TrackService
 
 @Singleton
-class BackendController @Inject() extends Controller {
+class BackendController @Inject()(
+    trackService: TrackService[TrackPoint])(implicit mat: Materializer)
+    extends Controller {
 
-  val clientUpdateV2: Reads[List[Unit]] = {
-    implicit val singleElement: Reads[Unit] = (
-        (JsPath \ "lat").read[Double](min(-90.0) keepAnd max(90.0)) and
-        (JsPath \ "lng").read[Double](min(-180.0) keepAnd max(180.0)) and
-        (JsPath \ "ele").read[Double](min(-1000.0) keepAnd max(8000.0)) and
-        (JsPath \ "heading").read[Double](min(0.0) keepAnd max(360.0)) and
-        (JsPath \ "timestamp").read[Int](min(0))
-    ).tupled.map(_ => ())
-
-    implicitly[Reads[List[Unit]]]
-  }
-
-  def send2(device: UUID) = Action(BodyParsers.parse.json(clientUpdateV2)) {
+  def send(device: UUID) = Action(BodyParsers.parse.json[List[TrackPoint]]) {
     request =>
+      request.body.foreach(trackService.consume(device, _))
+
       Ok
   }
 }
---
+++ test/services/TrackServiceTest.scala
@@ -0,0 +1,46 @@
+package services
+
+import java.util.UUID
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.testkit.scaladsl.TestSink
+import dao.FutureLinkedList
+import org.mockito.Matchers._
+import org.mockito.Mockito._
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, Matchers}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class TrackServiceTest extends FlatSpec with Matchers with ScalaFutures with MockitoSugar {
+
+  implicit val sys = ActorSystem()
+  implicit val mat = ActorMaterializer()
+
+  "Tracks service" should "pass-through all Points (first write than read)" in {
+    val linkedListMock = mock[FutureLinkedList[(UUID, Int)]]
+    val classUnderTest = new TrackServiceImpl[Int] {
+      override val linkedList = linkedListMock
+    }
+    val someId = UUID.randomUUID()
+
+    val givenPoints = List(1, 2, 3)
+
+
+    givenPoints.foreach(classUnderTest.consume(someId, _))
+    verify(linkedListMock, times(givenPoints.size)).append(any())
+  }
+
+  "Tracks service allTracks" should "never terminate" in {
+    val classUnderTest = new TrackServiceImpl[Int]
+
+    val probe = classUnderTest.allTracks.runWith(TestSink.probe[(UUID, Int)])
+
+    probe.ensureSubscription()
+    probe.request(2)
+    probe.expectNoMsg()
+  }
+
+}
