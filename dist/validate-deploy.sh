550b52b1d36750ac28513b818979a45cbf927663
Johntoenail <johntoenail@users.noreply.github.com>
2016-08-23 12:25:34 +0200

    QA-38: Modified validate-deploy.sh to be commandline capable.
     - Also, added bash sanity
    
    Change-Id: Ieefbb08a75d4973b43f2443032976cc4c02f63a5

--- dist/validate-deploy.sh
+++ dist/validate-deploy.sh
@@ -4,25 +4,32 @@ echo "Validating install"
 #HOSTNAME=shiptrail.lenucksi.eu
 HOSTNAME=$1
 
+SERVES_PLAY_RESULT=false
 SERVES_PLAY=`curl -k http://$HOSTNAME/ 2>/dev/null | grep "Welcome to Play" -c`
 if [ "$SERVES_PLAY" -ge "1" ]; then
-        echo "/ Serves Play BE OK"
+    echo "/ Serves Play BE OK"
+    SERVES_PLAY_RESULT=true
     true
 else
-        echo "/ Serves Play BE FAIL"
+    echo "/ Serves Play BE FAIL"
+    SERVES_PLAY_RESULT=false
     false
 fi
 
 SERVES_FE=`curl -L -v -k http://$HOSTNAME/fe/ 2>/dev/null | grep "ng-app=gulpAngular" -c`
 if [ "$SERVES_FE" -ge "1" ]; then
-        echo "/fe/ Serves FE OK"
+    echo "/fe/ Serves FE OK"
+    SERVES_FE_RESULT=true
     true
 else
-        echo "/fe/ Serves FE FAIL"
+    echo "/fe/ Serves FE FAIL"
+    SERVES_FE_RESULT=false
     false
 fi
 
 cd /tmp
 curl -O -L -v -k http://$HOSTNAME/swpdvtracker.apk 2>/dev/null
-ls swpdvtracker.apk && echo "Serves Android APK OK"
+test -f swpdvtracker.apk && echo "Serves Android APK OK"
 rm -v swpdvtracker.apk || true
+
+$SERVES_PLAY_RESULT && $SERVES_FE_RESULT && `test -f swpdvtracker.apk`
