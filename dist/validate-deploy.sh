b4369da684235d542745be2b4494a5d1398bd2af
Johntoenail <johntoenail@users.noreply.github.com>
2016-08-23 16:24:16 +0200

    QA-39: Cleaner APK validation handling
    Change-Id: I9c9663a8d2ac1961bec55f9b70528464ea5c6e4d

--- dist/validate-deploy.sh
+++ dist/validate-deploy.sh
@@ -22,14 +22,30 @@ if [ "$SERVES_FE" -ge "1" ]; then
     SERVES_FE_RESULT=true
     true
 else
-    echo "/fe/ Serves FE FAIL"
+echo "/fe/ Serves FE FAIL"
     SERVES_FE_RESULT=false
     false
 fi
 
 cd /tmp
 curl -O -L -v -k http://$HOSTNAME/swpdvtracker.apk 2>/dev/null
-test -f swpdvtracker.apk && echo "Serves Android APK OK"
+
+SERVES_APK_RESULT=false
+if [ -f 'swpdvtracker.apk' ]; then
+	echo "Serves Android APK OK"
+	SERVES_APK_RESULT=true
+	true
+else
+	echo "Serves Android APK FAIL"
+	SERVES_APK_RESULT=false
+	false
+fi
+
+#test -f swpdvtracker.apk && echo "Serves Android APK OK"
 rm -v swpdvtracker.apk || true
 
-$SERVES_PLAY_RESULT && $SERVES_FE_RESULT && `test -f swpdvtracker.apk`
+echo play result: $SERVES_PLAY_RESULT
+echo fe result: $SERVES_FE_RESULT
+echo apk result: $SERVES_APK_RESULT
+
+$SERVES_PLAY_RESULT && $SERVES_FE_RESULT && $SERVES_APK_RESULT
