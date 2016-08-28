663020d2500e0364e076d51eb19f8db47de71330
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2016-08-28 19:06:40 +0200

    BE-26: enhanced client <-> backend API (added additional data)
    Change-Id: I41eaf4c9a3919335f7d76290f7fe634d738a1c41
    See: https://belize.imp.fu-berlin.de/confluence/display/BE/Sensor-Client+%3C-%3E+Backend-API

--- app/models/TrackPoint.scala
+++ app/models/TrackPoint.scala
@@ -6,18 +6,37 @@ import play.api.libs.json.{JsPath, _}
 
 case class TrackPoint(lat: Double,
                       lng: Double,
-                      ele: Double,
-                      heading: Double,
-                      timestamp: Int)
+                      timestamp: Int,
+                      ele: Option[Double],
+                      gpsMeta: Option[Seq[GpsMeta]],
+                      compass: Option[Seq[Compass]],
+                      accelerometer: Option[Seq[Accelerometer]],
+                      orientation: Option[Seq[Orientation]])
+
+case class Compass(deg: Double, toffset: Int)
+
+case class Accelerometer(x: Double, y: Double, z: Double, toffset: Int)
+
+case class Orientation(azimuth: Double, pitch: Double, roll: Double, toffset: Int)
+
+case class GpsMeta(accuracy: Double, satCount: Int, toffset: Int)
 
 object TrackPoint {
+  implicit val compassFormat = Json.format[Compass]
+  implicit val accelerometerFormat = Json.format[Accelerometer]
+  implicit val orientationFormat = Json.format[Orientation]
+  implicit val gpsMetaFormat = Json.format[GpsMeta]
+
   implicit val trackPointFormat: Format[TrackPoint] = {
     (
-        (JsPath \ "lat").format[Double](min(-90.0) keepAnd max(90.0)) and
-        (JsPath \ "lng").format[Double](min(-180.0) keepAnd max(180.0)) and
-        (JsPath \ "ele").format[Double](min(-1000.0) keepAnd max(8000.0)) and
-        (JsPath \ "heading").format[Double](min(0.0) keepAnd max(360.0)) and
-        (JsPath \ "timestamp").format[Int](min(0))
-    )(TrackPoint.apply, unlift(TrackPoint.unapply))
+      (JsPath \ "lat").format[Double](min(-90.0) keepAnd max(90.0)) and
+      (JsPath \ "lng").format[Double](min(-180.0) keepAnd max(180.0)) and
+      (JsPath \ "timestamp").format[Int](min(0)) and
+      (JsPath \ "ele").formatNullable[Double] and
+      (JsPath \ "gpsMeta").formatNullable[Seq[GpsMeta]] and
+      (JsPath \ "compass").formatNullable[Seq[Compass]] and
+      (JsPath \ "accelerometer").formatNullable[Seq[Accelerometer]] and
+      (JsPath \ "orientation").formatNullable[Seq[Orientation]]
+      ) (TrackPoint.apply, unlift(TrackPoint.unapply))
   }
 }
