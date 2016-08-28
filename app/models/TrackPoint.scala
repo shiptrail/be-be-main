cd004e5971f71ae833f703264d9da092345ea543
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2016-08-28 19:06:40 +0200

    BE-26: enhanced client <-> backend API (added additional data)
    Change-Id: I41eaf4c9a3919335f7d76290f7fe634d738a1c41
    See: https://belize.imp.fu-berlin.de/confluence/display/BE/Sensor-Client+%3C-%3E+Backend-API

--- app/models/TrackPoint.scala
+++ app/models/TrackPoint.scala
@@ -1,23 +1,57 @@
 package models
-
+import io.github.karols.units.SI.Short._
+import io.github.karols.units._
+import models.TrackPoint.{Accelerometer, Compass, GpsMeta, Orientation}
+import models.UnitHelper._
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
 import play.api.libs.json.{JsPath, _}
 
 case class TrackPoint(lat: Double,
                       lng: Double,
-                      ele: Double,
-                      heading: Double,
-                      timestamp: Int)
+                      timestamp: IntU[ms],
+                      ele: Option[Double],
+                      gpsMeta: Seq[GpsMeta],
+                      compass: Seq[Compass],
+                      accelerometer: Seq[Accelerometer],
+                      orientation: Seq[Orientation])
 
 object TrackPoint {
+
+  case class Compass(deg: Double, toffset: Int)
+  case class Accelerometer(x: DoubleU[m / (s ^ _2)],
+                           y: DoubleU[m / (s ^ _2)],
+                           z: DoubleU[m / (s ^ _2)],
+                           toffset: IntU[ms])
+  case class Orientation(azimuth: Double,
+                         pitch: Double,
+                         roll: Double,
+                         toffset: IntU[ms])
+  case class GpsMeta(accuracy: Double, satCount: Int, toffset: IntU[ms])
+
+  implicit val gpsMetaFormat = Json.format[GpsMeta]
+  implicit val compassFormat = Json.format[Compass]
+  implicit val accelerometerFormat = Json.format[Accelerometer]
+  implicit val orientationFormat = Json.format[Orientation]
+
+  def optionalSeq[T: Format](path: JsPath): OFormat[Seq[T]] =
+    path
+      .formatNullable[Seq[T]]
+      .inmap(
+          o => o.getOrElse(Seq.empty),
+          s => Some(s)
+      )
+
   implicit val trackPointFormat: Format[TrackPoint] = {
     (
         (JsPath \ "lat").format[Double](min(-90.0) keepAnd max(90.0)) and
         (JsPath \ "lng").format[Double](min(-180.0) keepAnd max(180.0)) and
-        (JsPath \ "ele").format[Double](min(-1000.0) keepAnd max(8000.0)) and
-        (JsPath \ "heading").format[Double](min(0.0) keepAnd max(360.0)) and
-        (JsPath \ "timestamp").format[Int](min(0))
+        (JsPath \ "timestamp").format[IntU[ms]] and
+        (JsPath \ "ele").formatNullable[Double] and
+        optionalSeq[GpsMeta](JsPath \ "gpsMeta") and
+        optionalSeq[Compass](JsPath \ "compass") and
+        optionalSeq[Accelerometer](JsPath \ "accelerometer") and
+        optionalSeq[Orientation](JsPath \ "orientation")
     )(TrackPoint.apply, unlift(TrackPoint.unapply))
   }
 }
