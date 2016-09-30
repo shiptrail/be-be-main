a4fdd5da9f45cf76210a9906e531f8564f77e972
Mehmed Halilovic <mehmed.h.93@googlemail.com>
2016-09-30 14:33:45 +0200

    BE-31 Application loads TrackPoints from a certain location at the start of the application.
    Change-Id: I2e2147d9edf4241c6e0142b3d7591b11f9680d27

---
+++ app/services/parser/MultiFormatParser.scala
@@ -0,0 +1,32 @@
+package services
+
+import java.io.File
+
+import models.TrackPoint
+import services.parser.{FrontendFileParser, GpsTrackParser, GpxParser}
+
+/**
+  * Copied from influx-simulator and edited
+  */
+object MultiFormatParser {
+
+  val parserForExtension = Map(
+      "gpx" -> GpxParser,
+      "cgps" -> CustomFormatParser,
+      "fps" -> FrontendFileParser
+  )
+
+  def parse(file: File): Iterator[TrackPoint] = {
+    parserForFileName(file) match {
+      case Some(parserForFile) => parserForFile.parse(file)
+      case None =>
+        System.err.println("Error: Unsupported file type!")
+        Iterator.empty
+    }
+  }
+
+  def parserForFileName(file: File): Option[GpsTrackParser] = {
+    val fileExtension = file.getName.split('.').last
+    parserForExtension.get(fileExtension)
+  }
+}
